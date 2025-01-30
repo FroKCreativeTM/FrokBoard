3b4ee672c9457b1cecbaee779e55d3b3c914f10e
seungCheolCha <cscculture95@gmail.com>
2025-01-30 14:28:19 +0900

    BoardCommon의 코드를 테스트하기 위한 xUnit 테스트 프로젝트 추가 및 SnowFlake 알고리즘 테스트

---
+++ CommonTest/SnowflakeIdGeneratorTests.cs
@@ -0,0 +1,50 @@
+﻿using BoardCommon.Snowflake;
+
+namespace CommonTest;
+
+public class SnowflakeIdGeneratorTests
+{
+    private readonly SnowflakeIdGenerator _idGenerator;
+
+    public SnowflakeIdGeneratorTests()
+    {
+        _idGenerator = new SnowflakeIdGenerator(1);
+    }
+
+    [Fact]
+    public void GenerateUniqueIds_ShouldReturnDifferentValues()
+    {
+        long id1 = _idGenerator.NextId();
+        long id2 = _idGenerator.NextId();
+
+        Assert.NotEqual(id1, id2);
+    }
+
+    [Fact]
+    public void GenerateId_ShouldBePositive()
+    {
+        long id = _idGenerator.NextId();
+        Assert.True(id > 0);
+    }
+
+    [Fact]
+    public void GenerateId_ShouldIncreaseOverTime()
+    {
+        long id1 = _idGenerator.NextId();
+        System.Threading.Thread.Sleep(1);
+        long id2 = _idGenerator.NextId();
+
+        Assert.True(id2 > id1);
+    }
+
+    [Fact]
+    public void DecodeId_ShouldExtractCorrectValues()
+    {
+        long id = _idGenerator.NextId();
+        var (timestamp, nodeId, sequence) = SnowflakeIdParser.Decode(id);
+
+        Assert.True(timestamp > new DateTime(2020, 1, 1));
+        Assert.Equal(1, nodeId);
+        Assert.True(sequence >= 0);
+    }
+}
